6605e7b71010ed64e2538886a6bcb13a843040bc
Yannick Gerber <varmox@protonmail.ch>
2020-09-29 13:44:37 +0200

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> //lib for malloc, calloc, ralloc
+
+//malloc() unitialized
+
+
+
+int main (void) {
+
+	//malloc
+	char *name = malloc(8* sizeof(char)); 	//provide 8 bytes (yannick = 7 + \0)
+	if (name == NULL)						//check if char equals NULL, if yes print error and close programm
+	{
+		printf("ERROR: \n");
+		return 1;
+	}
+	strcpy(name, "yannick");
+
+	
+
+}
